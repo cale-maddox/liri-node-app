adc29ab615bd1008c5f1772cc7a24ed0993ef0a5
cale maddox <calemaddox@cales-MacBook-Pro.local>
2017-03-30 17:01:51 -0400

    added spotify and twitter

--- liri.js
+++ liri.js
@@ -46,9 +46,58 @@ if (nodeArg[2]==="movie-this") {
 }
 else if (nodeArg[2]==="spotify-this-song") {
 	console.log("yay!");
+
+	var SpotifyWebApi = require('spotify-web-api-node');
+
+	var songArr =[];
+
+	var songTitle = "";
+
+	for (i=3; i<nodeArg.length; i++){
+		songArr.push(nodeArg[i]);
+	};
+
+
+	songTitle = songArr.join('+');
+	console.log(songTitle);
+
+	var spotify = require('spotify');
+ 
+	spotify.search({ type: 'track', query: songTitle }, function(err, data) {
+    	if ( err ) {
+        	console.log('Error occurred: ' + err);
+        	return;
+    	}
+ 		
+ 		console.log(data.tracks.items[0]);
+    	// Do something with 'data' 
+	});
+
+
 }
 else if (nodeArg[2]==="my-tweets") {
-	console.log("yay!");		
+	console.log("yay!");
+
+	var Twitter = require('twitter');
+
+	var client = new Twitter({
+  		consumer_key: ck,
+  		consumer_secret: cs,
+  		access_token_key: tk,
+  		access_token_secret: ts
+	});	
+
+	client.get('statuses/user_timeline', {screen_name: 'CaleMaddox', count: 20,  exclude_replies: true}, function(error, tweets, response) {
+  		if(error) throw error;
+  		// console.log(tweets[0].created_at);  // The favorites.
+  		// console.log(tweets[0].text);  // Raw response object.
+
+  		for(var i=0; i<tweets.length; i++){
+  			console.log("************"+"\nTweet: "+tweets[i].text+"\nPosted: "+tweets[i].created_at);
+  		}
+
+	});	
+
 }
 else if (nodeArg[2]==="do-what-it-says") {
 	console.log("yay!");
